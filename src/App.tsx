48575fbb3b26321362347d3b921d5624970b9b34
Marcus Gambogi <marcusgambogi10@gmail.com>
2023-12-07 19:19:46 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://http2.mlstatic.com/miniatura-batmovel-batman-89-132-jada-D_NQ_NP_901608-MLB25684227825_062017-F.jpg">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,30 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+    import { fireEvent, render, screen } from '@testing-library/react';
+    import Post from '.';
+    import PostComment from '.';
+    import userEvent from '@testing-library/user-event';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+
+    describe('Teste para o componente PostComment', () => {
+        it('Deve renderizar o componente corretamente', () => {
+            render(<PostComment/>);
+            expect(screen.getByText('Comentar')).toBeInTheDocument();
+        });
+        test('inserir dois comentários', () => {
+            render(<Post />);
+            
+            const textarea = screen.getByTestId('post-comments-form-textarea');
+            const button = screen.getByTestId('post-comments-form-button');
+            userEvent.type(textarea, 'Este é o primeiro comentário');
+            fireEvent.click(button);
+        
+            const primeiroComentario = screen.getByText('Este é o primeiro comentário');
+            expect(primeiroComentario).toBeInTheDocument();
+        
+            userEvent.type(textarea, 'Este é o segundo comentário');
+            fireEvent.click(button);
+        
+            const segundoComentario = screen.getByText('Este é o segundo comentário');
+            expect(segundoComentario).toBeInTheDocument();
+        });
+        });
+        
